03063b9d01ff062565f6bc1089751f70bd4b92f0
JULIO CESAR SANJUAN HERNANDEZ <sahj@live.com.mx>
2022-03-09 22:33:54 -0600

    Portada de la aplicación -  boceto

---
+++ index.js
@@ -0,0 +1,45 @@
+const presentacion = document.getElementById("presentacion");
+
+const anthem = `Mexicanos, al grito de guerra
+El acero aprestad y el bridón
+Y retiemble en sus centros la Tierra
+Al sonoro rugir del cañón
+Y retiemble en sus centros la Tierra
+Al sonoro rugir del cañón`;
+
+let lines = anthem.split("\n");
+
+const finalParagraph = lines.join("<br>");
+
+const fecha = document.getElementById("fecha");
+
+const now = new Date();
+
+const months = ["ene", "feb", "mzo", "apr", "may", "jun", "jul","sep"];
+
+const fullDate = `${now.getDate()}-${months[now.getMonth()]}-${now.getFullYear()}`;
+
+fecha.innerHTML = fullDate;
+
+presentacion.innerHTML = finalParagraph;
+
+const menu = document.getElementById("menu");
+
+const opciones= ["Inicio", "Productos","Carrito"];
+
+let menuOps = "<li>"+opciones.join("</li><li>")+"</li>";
+
+let template = '';
+for (const opcion of opciones) {
+    template += `<li>${opcion}</li>`;
+}
+menu.innerHTML = template;
+
+menu.innerHTML = "";
+for (const opcion of opciones) {
+    //let template = `<li>${opcion}</li>`;
+    let li = document.createElement("li");
+    li.innerHTML = opcion;
+    menu.appendChild( li );
+}
+
