93f72632d2bd62c297058eeb0a988763509347ac
JULIO CESAR SANJUAN HERNANDEZ <sahj@live.com.mx>
2022-03-25 09:25:18 -0600

    ACTIVIDAD 24 MAR

--- controllers/cart.controller.js
+++ controllers/cart.controller.js
@@ -1,15 +1,14 @@
-/*localStorage.setItem("cart", JSON.stringify([
-    {
-        sku : "CURJSDC",
-        cantidad: 2,
-        descuento: 1
-    },
-    {
-        sku : "CURNODEJSDC",
-        cantidad: 1,
-        descuento: 0
-    }
-])); */
+/** ACTIVIDADES SUGERIDAS
+ * 1 - importar la clase Carrito y ListaProductos para poder acceder a la lista de productos y al carrito, o las que sean necesarias.
+ * 2 - instanciar los objetos de las clases importadas
+ * 3 - Eliminar de este modulo todo aquello que no sea su responsabilidad
+ * 4 - Implementar la lógica para que funcione el carrito
+ * 5 - Crear alguna nueva clase si lo consideran necesario
+ * 6 - Hacer limpieza de código y hacer la documentación como se hiso en /models/response.class.js
+ * 
+ * RESULTADO ESPERADO
+ * Se debe ver el desgloce del carrito de compras como ya se tenía antes de usar POO
+ */
 
 let cart = localStorage.getItem("cart");
 
@@ -47,8 +46,7 @@ const cartContainer = document.getElementById("products_on_cart")
 
 
 for (const item of cartAsJson) {
-        
-    //let prod = findProduct(item.sku);
+    let prod = findProduct(item.sku);
     console.log("prod", prod)
     const newRow = getRowLayout( [
         getProductImage(prod.imagen),
